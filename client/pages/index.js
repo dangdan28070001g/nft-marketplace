533ae6038d2a4c2cc591c0ed34b4b18b796794fa
dangdan28070001g <dangdan28070001+github@gmail.com>
2023-03-19 10:38:47 +0700

    first commit

---
+++ client/pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className='text-xl text-red-400'>Hello world</h1>
+    </div>
+  )
+}
